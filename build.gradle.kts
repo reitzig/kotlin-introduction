96996173c28cf78da9d95cdfe434ac0cdcee8a1f
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2019-06-21 22:46:10 +0200

    Bump Gradle wrapper to 5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,18 @@ plugins {
     kotlin("jvm") version "1.3.40"
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
-
 repositories {
     mavenCentral()
 }
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
 }
 
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
 }
